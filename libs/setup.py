c5dea4bf2407b1e8f5ffe8a1802e9e96d5781049
Dmitry Kisler <admin@dkisler.com>
2020-12-13 04:25:46 +0100

    up

---
+++ libs/setup.py
@@ -0,0 +1,45 @@
+# Copyright 2020 dkisler.com Dmitry Kisler
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+# NONINFRINGEMENT. IN NO EVENT WILL THE LICENSOR OR OTHER CONTRIBUTORS
+# BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""ServerlessML package."""
+from pathlib import Path
+from setuptools import setup, find_namespace_packages  # type: ignore
+
+
+DIR = Path(__file__).parent
+requirements = (DIR / "requirements.txt").read_text()
+
+setup(
+    name="etl",
+    version="1.0.0",
+    description="ServerlessML core package.",
+    url="https://github.com/",
+    author="Dmitry Kisler",
+    author_email="admin@dkisler.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Development Status :: 2 - Alpha",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_namespace_packages(where=".", exclude=("test",)),
+    install_requires=requirements,
+)
