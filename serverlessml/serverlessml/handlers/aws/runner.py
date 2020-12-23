5f0f13fc9a014d83b3d8939ce371d12049cb5092
Dmitry Kisler <admin@dkisler.com>
2020-12-23 10:52:59 +0100

    adds aws runner

--- serverlessml/serverlessml/handlers/aws/runner.py
+++ serverlessml/serverlessml/handlers/aws/runner.py
@@ -21,12 +21,53 @@
 with the runner's logic to be executed on AWS.
 """
 
+import json
+import logging
+import sys
+from functools import partial
+from typing import Any, Dict
 
-def run(project_id: str, port: int) -> None:
+from serverlessml import ControllerIO, Runner
+from serverlessml.errors import ModelDefinitionError, PipelineConfigError, PipelineRunningError
+
+
+def run(event: Dict[str, Any], context: Any) -> None:  # pylint: disable=unused-argument
     """Contains the end-to-end logic to run pipelines.
 
     Args:
-        project_id: Model project ID.
-        port: Port for the web-server to listen.
+        event: AWS lambda event object.
+        context: AWS lambda context.
     """
-    print(project_id, port)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(linenum)d] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    log = logging.getLogger(__name__)
+    log.debug("Invoke serverlessML pipeline with %s", __name__)
+
+    record: Dict[str, Any] = event["Records"][0]["Sns"]
+    topic: str = record["TopicArn"]
+
+    try:
+        payload: Dict[str, Any] = json.loads(record["Message"])
+    except json.JSONDecodeError as ex:
+        log.error(ex)
+        sys.exit(1)
+
+    try:
+        runner = Runner(
+            partial(ControllerIO, project_id=payload.get("project_id", ""), platform="aws")
+        )
+        if topic.endswith("train"):
+            runner.train(payload)
+        elif topic.endswith("predict"):
+            runner.predict(payload)
+        else:
+            log.error("Triggered by the wrong topic.")
+            sys.exit(1)
+    except (ModelDefinitionError, PipelineConfigError, PipelineRunningError) as ex:
+        log.error(ex)
+        sys.exit(1)
+
+    log.debug("Done")
